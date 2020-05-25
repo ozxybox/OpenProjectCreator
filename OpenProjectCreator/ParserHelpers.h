cc0471c5e0d39706ee5694f17bfa16e5aed6ccd5
ozxybox <kieferstashu@gmail.com>
2020-05-25 02:47:08 -0500

    Add helper char checking functions

--- OpenProjectCreator/ParserHelpers.h
+++ OpenProjectCreator/ParserHelpers.h
@@ -1,2 +1,36 @@
 #pragma once
 
+inline bool IsWhitespace(char c)
+{
+	return c <= ' ' || c == 127;
+}
+
+inline bool IsNumber(char c)
+{
+	return c >= '0' && c <= '9';
+}
+
+inline bool IsNumber(char c)
+{
+	return c >= '0' && c <= '9';
+}
+
+inline bool IsLowercase(char c)
+{
+	return c >= 'a' && c <= 'z';
+}
+
+inline bool IsUppercase(char c)
+{
+	return c >= 'A' && c <= 'Z';
+}
+
+inline bool IsSymbol(char c)
+{
+	return (c >= '!' && c <= '/') || (c >= ':' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~');
+}
+
+inline bool IsAlphanumerical(char c)
+{
+	return IsNumber(c) || IsUppercase(c) || IsLowercase(c);
+}
