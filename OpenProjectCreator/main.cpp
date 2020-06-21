6aa87b8824d60f25e07877d21d0e443be1a1bd64
ozxybox <kieferstashu@gmail.com>
2020-06-21 04:55:18 -0500

    Add a basic vpc and the ability to read it

--- OpenProjectCreator/main.cpp
+++ OpenProjectCreator/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "VPCParser.h"
+
+char* ReadFile(const char* path, size_t& len)
+{
+
+	FILE* f;
+	errno_t error = fopen_s(&f, path, "rb");
+	fseek(f, 0, SEEK_END);
+	len = ftell(f);
+	rewind(f);
+	char* str = (char*)malloc(len + 1);
+	str[len] = 0;
+	fread(str, len, 1, f);
+	fclose(f);
+
+	return str;
+}
+
+
 
 int main(int argc, char** argv)
 {
+
+	size_t len = 0;
+	const char* buf = ReadFile(argv[1], len);
+
+	VPCParser parser(buf, len);
 
 }
